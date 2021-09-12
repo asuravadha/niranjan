47bc2ea06198e23f1d3e1a84736f98ea1a2a048c
asuravadha <rr.easwar@gmail.com>
2021-09-12 17:35:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function eid(name="employ",role="no_record " ){
+this.name=name;
+this.role=role;
+}
+
+eid.prototype.info=function(){
+  console.log(`${this.name} ${this.role}`)
+}
+
+var emp1=new eid("Eswar","web");
+
+var emp2=new eid("niranjan","web");
+
+var emp3=new eid("anjali","ba");
+
+var emp4=new eid();
+
+var emp5=new eid("kiran","ba");
+
+// console.log(`${emp1.name}  ${emp1.role}`)
+// console.log(`${emp2.name}  ${emp2.role}`)
+// console.log(`${emp3.name}  ${emp3.role}`)
+// console.log(`${emp4.name}  ${emp4.role}`)
+// console.log(`${emp5.name}  ${emp5.role}`)
+
+
+emp1.info();
+emp2.info();
+emp3.info();
+emp4.info();
+emp5.info();
